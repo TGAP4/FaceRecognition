52eb6869e28fe7169660c1d4678695ef22f45e07
TGAP4 <60671900+TGAP4@users.noreply.github.com>
2020-03-01 23:20:48 -0500

    Updates

---
+++ precache-manifest.8ef92f3bb251233f343857e5f28d27bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76aef580b7448656a63e621387d8bd43",
+    "url": "/TGAP4/FaceRecognition/index.html"
+  },
+  {
+    "revision": "781bab9b3a1f7043aef0",
+    "url": "/TGAP4/FaceRecognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "2227bbb3b594aec40beb",
+    "url": "/TGAP4/FaceRecognition/static/css/main.e9cbc8da.chunk.css"
+  },
+  {
+    "revision": "781bab9b3a1f7043aef0",
+    "url": "/TGAP4/FaceRecognition/static/js/2.15a10306.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/TGAP4/FaceRecognition/static/js/2.15a10306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2227bbb3b594aec40beb",
+    "url": "/TGAP4/FaceRecognition/static/js/main.48d4b134.chunk.js"
+  },
+  {
+    "revision": "a17bb1c884553acebb48",
+    "url": "/TGAP4/FaceRecognition/static/js/runtime-main.20438dcd.js"
+  }
+]);
