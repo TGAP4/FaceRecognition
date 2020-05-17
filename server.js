21a8468a2baaeb08e1e47efc90644c55cd343f7e
TGAP4 <60671900+TGAP4@users.noreply.github.com>
2020-05-17 03:53:43 -0400

    restructured file structure

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(express.static('client/'));
+app.use(express.static('build/'));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.get('/', (req, res) => res.json(db.users));
